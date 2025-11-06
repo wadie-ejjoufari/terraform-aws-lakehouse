01faf55c4a60dbac2c60d4fcc8996a2c6ebbafaf
wadie-ejjoufari <wadie.ejjoufari@gmail.com>
2025-11-06 17:04:59 +0000

    AWS Glue and Athena resources

---
+++ modules/catalog_athena/main.tf
@@ -0,0 +1,149 @@
+terraform {
+  required_version = ">= 1.8.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+# Glue database for lakehouse
+resource "aws_glue_catalog_database" "db" {
+  name = replace("${var.name_prefix}-lake", "_", "-")
+}
+
+# Athena workgroup with encrypted, per-env output location
+resource "aws_athena_workgroup" "wg" {
+  name  = "${var.name_prefix}-wg"
+  state = "ENABLED"
+  configuration {
+    enforce_workgroup_configuration = true
+    result_configuration {
+      output_location = "s3://${var.athena_results_bucket}/athena-results/"
+      encryption_configuration {
+        encryption_option = "SSE_KMS"
+        kms_key_arn       = var.kms_key_arn
+      }
+    }
+  }
+  tags = var.tags
+}
+# Bronze external table (partition projection, no crawler, schema defined manually)
+# Data expected under s3://<raw_bucket>/github/events/ingest_dt=YYYY-MM-DD/
+resource "aws_glue_catalog_table" "github_events_bronze" {
+  name          = "github_events_bronze"
+  database_name = aws_glue_catalog_database.db.name
+  table_type    = "EXTERNAL_TABLE"
+  parameters = {
+    classification                = "json"
+    "projection.enabled"          = "true"
+    "projection.ingest_dt.format" = "yyyy-MM-dd"
+    "projection.ingest_dt.type"   = "date"
+    "storage.location.template"   = "s3://${var.raw_bucket}/github/events/ingest_dt=$${ingest_dt}/"
+    has_encrypted_data            = "true"
+  }
+  storage_descriptor {
+    location      = "s3://${var.raw_bucket}/github/events/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "json"
+      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+    }
+
+    columns {
+      name = "id"
+      type = "string"
+    }
+    columns {
+      name = "type"
+      type = "string"
+    }
+    columns {
+      name = "created_at"
+      type = "string"
+    }
+    columns {
+      name = "repo_id"
+      type = "bigint"
+    }
+    columns {
+      name = "repo_name"
+      type = "string"
+    }
+    columns {
+      name = "actor_id"
+      type = "bigint"
+    }
+    columns {
+      name = "actor_login"
+      type = "string"
+    }
+    columns {
+      name = "payload_raw"
+      type = "string"
+    }
+
+    compressed = false
+  }
+
+  partition_keys {
+    name = "ingest_dt"
+    type = "string"
+  }
+}
+
+# Silver table (CTAS-managed, Parquet format)
+resource "aws_glue_catalog_table" "github_events_silver" {
+  name          = "github_events_silver"
+  database_name = aws_glue_catalog_database.db.name
+  table_type    = "EXTERNAL_TABLE"
+
+  parameters = {
+    classification     = "parquet"
+    has_encrypted_data = "true"
+  }
+
+  storage_descriptor {
+    location      = "s3://${var.silver_bucket}/github/events_silver/"
+    input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+
+    ser_de_info {
+      name                  = "parquet"
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+    }
+
+    columns {
+      name = "event_id"
+      type = "string"
+    }
+    columns {
+      name = "event_type"
+      type = "string"
+    }
+    columns {
+      name = "event_date"
+      type = "date"
+    }
+    columns {
+      name = "repo_name"
+      type = "string"
+    }
+    columns {
+      name = "actor_login"
+      type = "string"
+    }
+  }
+
+  partition_keys {
+    name = "year"
+    type = "string"
+  }
+  partition_keys {
+    name = "month"
+    type = "string"
+  }
+}
---
+++ modules/catalog_athena/variables.tf
@@ -0,0 +1,29 @@
+variable "name_prefix" {
+  description = "Prefix for naming resources, e.g., 'dp-dev'"
+  type        = string
+}
+
+variable "raw_bucket" {
+  description = "S3 bucket name for raw/bronze layer data"
+  type        = string
+}
+
+variable "silver_bucket" {
+  description = "S3 bucket name for silver layer data"
+  type        = string
+}
+
+variable "athena_results_bucket" {
+  description = "S3 bucket name for Athena query results"
+  type        = string
+}
+
+variable "kms_key_arn" {
+  description = "ARN of the KMS key for encryption"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+}
