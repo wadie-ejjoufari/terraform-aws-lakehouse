85cf5812562a5cd17eb951795277f1b03a8dbfab
wadie-ejjoufari <wadie.ejjoufari@gmail.com>
2025-11-16 12:12:57 +0000

    signing permissions to IAM policy

--- global/iam_gh_oidc/main.tf
+++ global/iam_gh_oidc/main.tf
@@ -42,7 +42,7 @@ locals {
         "ec2:Describe*",
         "logs:Describe*", "logs:List*", "cloudwatch:Describe*",
         "events:Describe*", "events:List*",
-        "lambda:GetFunction", "lambda:List*",
+        "lambda:GetFunction", "lambda:GetFunctionCodeSigningConfig", "lambda:GetPolicy", "lambda:List*",
         "glue:Get*", "glue:List*", "athena:Get*", "athena:List*",
         "iam:List*", "iam:Get*",
         "s3:GetBucketPublicAccessBlock", "s3:GetBucketVersioning",
